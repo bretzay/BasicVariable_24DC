1e0a5f09c6f7438f19dda456da30552641cae384
mthomas <matheot72@gmail.com>
2025-03-22 15:43:43 +0100

    agent de traitement

--- controller/mistral.py
+++ controller/mistral.py
@@ -6,12 +6,13 @@ var=input("Avez-vous des question ?")
 load_dotenv()
 
 api_key = os.getenv("MISTRAL_API_KEY")
-agent_id = os.getenv("MISTRAL_AGENT_ID")
+agent_reponse_id = os.getenv("MISTRAL_AGENT_REPONSE_ID")
+agent_json_id = os.getenv("MISTRAL_AGENT_JSON_ID")
 
 client = Mistral(api_key=api_key)
 
 chat_response = client.agents.complete(
-    agent_id=agent_id,
+    agent_id=agent_reponse_id,
     messages=[
         {
             "role": "user",
