7770ac24ce0255873067ecda479d075a3cb1d7e4
bretzay <aurelien.damoizeau@gmail.com>
2025-03-22 15:03:33 +0100

    Add Env Variables Move Requirements & rename

--- controller/mistral.py
+++ controller/mistral.py
@@ -1,12 +1,16 @@
 import os
+from dotenv import load_dotenv
 from mistralai import Mistral
 
-api_key = os.environ["MISTRAL_API_KEY"]
+load_dotenv()
+
+api_key = os.getenv("MISTRAL_API_KEY")
+agent_id = os.getenv("MISTRAL_AGENT_ID")
 
 client = Mistral(api_key=api_key)
 
 chat_response = client.agents.complete(
-    agent_id="",
+    agent_id=agent_id,
     messages=[
         {
             "role": "user",
